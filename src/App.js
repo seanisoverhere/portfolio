d95850bdedabcabff6c3e86dcb108374fd2ca343
Sean <sean_choon@hotmail.com>
2021-05-18 10:27:53 +0800

    Include headers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
+import Headers from './components/Headers';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Headers />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
